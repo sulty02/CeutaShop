7b0a1196000097203323fb1504dac55abd64e4a1
Jorge-DAM-Ceuta <jorgemugar12@hotmail.com>
2024-02-19 19:20:37 +0100

    Confirmacion carrito

--- View/Assets/JS/Confirmaciones.js
+++ View/Assets/JS/Confirmaciones.js
@@ -34,22 +34,22 @@ function confirmarEliminarProducto(idProducto) {
   };
 }
 
-function comprobarSesion(idProducto, idNegocio, unidades){
+function comprobarSesion(idProducto, idNegocio, unidades, comp){
   var sesionModal = document.getElementById("sesionModal");
   var sesionMensaje = document.getElementById("sesionMensaje");
   var confirmarSesionBtn = document.getElementById("confirmarSesionBtn");
 
-  if(<?php echo isset($_SESSION['usuario']) ? 'true' : 'false'; ?>){
+  if(comp == true){
     //La sesión está seteada, redirige a Controller/AniadirAlCarrito.php
     window.location.href = "Controller/AniadirAlCarrito.php?idProducto=" + idProducto + "&idNegocio=" + idNegocio + "&unidades=" + unidades;
-  }else{
-    //La sesión no está seteada, muestra el modal
-    sesionMensaje.innerHTML = "Para añadir un producto al carrito debes iniciar sesión";
-    sesionModal.style.display = "block";
-
-    /Función para cerrar el modal
-    confirmarSesionBtn.onclick = function(){
-      sesionModal.style.display = "none";
-    };
   }
+
+  //La sesión no está seteada, muestra el modal
+  sesionMensaje.innerHTML = "Para añadir un producto al carrito debes iniciar sesión";
+  sesionModal.style.display = "block";
+
+  confirmarSesionBtn.onclick = function(){
+    sesionModal.style.display = "none";
+  };
+  
 }
